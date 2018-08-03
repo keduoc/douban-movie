040c635e8c4f75c643c99d9afd06a3f2e72b9edf
keduoc <39652079+keduoc@users.noreply.github.com>
2018-08-03 18:04:01 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-var app = {
-    init: function(){
-        this.$tabs = $('footer>div')
-        this.$panels = $('section')
-        this.bind()
-
-        top250.init()
-        usBox.init()
-        search.init()
-
-    },
-    bind: function () {
-        var _this = this
-        this.$tabs.on('click',function () {
-            $(this).addClass('active').siblings().removeClass('active')
-            _this.$panels.eq($(this).index()).fadeIn().siblings().hide()
-        })
-    }
-}
-
-app.init()
 
 
 var top250 = {
@@ -294,7 +273,27 @@ var search = {
 
 
 
+var app = {
+    init: function(){
+        this.$tabs = $('footer>div')
+        this.$panels = $('section')
+        this.bind()
 
+        top250.init()
+        usBox.init()
+        search.init()
+
+    },
+    bind: function () {
+        var _this = this
+        this.$tabs.on('click',function () {
+            $(this).addClass('active').siblings().removeClass('active')
+            _this.$panels.eq($(this).index()).fadeIn().siblings().hide()
+        })
+    }
+}
+
+app.init()
 
 
 
